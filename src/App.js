1dd1be15ea18a6bfecdf43ce6a71f359a10aee81
Nabanita Paul <nabanitapaul@mail.usf.edu>
2020-09-04 15:17:52 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learns React
           </a>
         </header>
       </div>
